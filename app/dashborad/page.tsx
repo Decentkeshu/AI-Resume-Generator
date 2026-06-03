d18aa0499e3b03fba75e45797d3d697212dd4348
keshav kumar <msd76017@gmail.com>
2026-06-03 16:44:00 +0530

    dashboard update

--- app/dashborad/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-// export default async function DashboardPage (){
-//     const authObj = await auth();
-//     const userObj = await currentUser();
-//     console.log(authObj,userObj);
-
-//     return <h1>Dashboard</h1>
-// }
